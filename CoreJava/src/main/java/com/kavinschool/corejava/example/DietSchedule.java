c600e2e4d449097663d448febdab975bb52f5d75
Kangs Passoubady <kangs@kavinschool.com>
2024-10-02 12:28:34 -0400

    updated spelling

--- CoreJava/src/main/java/com/kavinschool/corejava/example/DietSchedule.java
+++ CoreJava/src/main/java/com/kavinschool/corejava/example/DietSchedule.java
@@ -10,12 +10,12 @@ public class DietSchedule {
             case 2: System.out.println("Tuesday");
             case 4: System.out.println("Thursday");
             case 6: System.out.println("Saturday");
-                    System.out.println(" Vegitarian");
+                    System.out.println(" Vegetarian");
                     break;
             case 3: System.out.println("Wednesday");
             case 5: System.out.println("Friday");
             case 7: System.out.println("Sunday");
-                    System.out.println(" Non Vegitarian");
+                    System.out.println(" Non Vegetarian");
                     break;
             default: System.out.println("Are you on earth?");
                     break;
