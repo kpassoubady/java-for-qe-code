6c98ba2dcb0b5aed54da768d72d83b9a8d289809
Kangs Passoubady <kangs@kavinschool.com>
2024-10-09 06:09:28 -0400

    Removed @Serial

--- CoreJava/src/main/java/com/kavinschool/shape/ColorRangeException.java
+++ CoreJava/src/main/java/com/kavinschool/shape/ColorRangeException.java
@@ -4,8 +4,6 @@
  */
 package com.kavinschool.shape;
 
-import java.io.Serial;
-
 /**
  * The Class ColorRangeException.
  *
@@ -13,17 +11,18 @@ import java.io.Serial;
  */
 public class ColorRangeException extends Exception {
 
-	/** The Constant serialVersionUID. */
-	@Serial
-	private static final long serialVersionUID = 8224930501456280322L;
+    /**
+     * The Constant serialVersionUID.
+     */
+    private static final long serialVersionUID = 8224930501456280322L;
 
-	/**
-	 * Instantiates a new color range exception.
-	 *
-	 * @param ErrorMsg the error msg
-	 */
-	public ColorRangeException(String ErrorMsg) {
-		super(ErrorMsg);
-	}
+    /**
+     * Instantiates a new color range exception.
+     *
+     * @param ErrorMsg the error msg
+     */
+    public ColorRangeException(String ErrorMsg) {
+        super(ErrorMsg);
+    }
 
 }
