488384d9d5b094fa2d8a8f3a4cfdcab34f922c6e
Kangs Passoubady <kangs@kavinschool.com>
2024-11-05 10:21:47 -0500

    updated 2d array example

--- CoreJava/src/main/java/com/kavinschool/arrays/MatrixArray.java
+++ CoreJava/src/main/java/com/kavinschool/arrays/MatrixArray.java
@@ -25,5 +25,24 @@ public class MatrixArray {
                 System.out.println("hourlyWeather = " + hourlyWeather);
             }
         }
+        System.out.println("====================================");
+
+        System.out.println("WeeksWeather[0][0] = " + WeeksWeather[0][0]);
+        System.out.println("WeeksWeather[0][1] = " + WeeksWeather[0][1]);
+        System.out.println("WeeksWeather[1][1] = " + WeeksWeather[1][1]);
+        System.out.println("WeeksWeather[6][23] = " + WeeksWeather[6][23]);
+
+        System.out.println("====================================");
+        System.out.println("WeeksWeather.length = " + WeeksWeather.length); // row length - 7
+        System.out.println("WeeksWeather[0].length = " + WeeksWeather[0].length); // column length - 24
+        System.out.println("====================================");
+
+        for (int i = 0; i < WeeksWeather.length; i++) {
+            System.out.printf("Day %d\n",i);
+            for (int j = 0; j < WeeksWeather[i].length; j++) {
+                System.out.printf("WeeksWeather[%d][%d] = %d\n", i, j, WeeksWeather[i][j]);
+            }
+        }
+        System.out.println("====================================");
     }
 }
